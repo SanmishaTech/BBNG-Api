42fb4dac21486759cabcd76cef972eddeb976b7a
yashchaud <yashchaudhari908@gmail.com>
2025-05-19 18:37:54 +0530

    asd

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,5 @@ module.exports = {
   appName: process.env.APP_NAME || "BBNG",
   defaultUserRole: process.env.DEFAULT_USER_ROLE || "user",
   allowRegistration: process.env.ALLOW_REGISTRATION || true,
-  frontendUrl: process.env.FRONTEND_URL || "localhost:5173",
+  frontendUrl: process.env.FRONTEND_URL || "15.207.30.113",
 };
